74019252591158f851d62eacfbf321bed6999fb6
jeremiah-joh <jeremiahjoh@sungkyul.ac.kr>
2023-07-27 07:08:39 +0900

    add list

--- hog4.py
+++ hog4.py
@@ -8,21 +8,20 @@ import math
 FACE_PATH = './KaggleDataSet/haarcascade_frontalface_default.xml'
 MOUTH_PATH = './KaggleDataSet/haarcascade_mcs_mouth.xml'
 EYE_PATH = cv2.data.haarcascades + 'haarcascade_eye.xml'
+MIN_EYE_OPEN_DIST = 5
+MIN_MOUTH_OPEN_DIST = 10
 
 # get frame of video and detect eyes and mouthes
 
 class ImageLabelPair:
 	def __init__(self, left_eye_label, left_eye_img, right_eye_label, right_eye_img, mouth_label, mouth_img):
 		self.left_eye_label = left_eye_label
-		self.lef_eye_img = left_eye_img
+		self.left_eye_img = left_eye_img
 		self.right_eye_label = right_eye_label
 		self.right_eye_img = right_eye_img
 		self.mouth_label = mouth_label
 		self.mouth_img = mouth_img
 		
-MIN_EYE_OPEN_DIST = 5
-MIN_MOUTH_OPEN_DIST = 10
-
 def get_faces(image, face_cascade):
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(gray, 1.3, 5)
@@ -56,21 +55,27 @@ def crop_eyes(image, faces, eyes_cascade):
     
     return left_eyes, right_eyes
 
+def crop_mouth(image, faces, mouth_cascade):
+    mouths = []
+
+    for (x, y, w, h) in faces:
+        roi_gray = image[y:y+h, x:x+w]
+        mouths.append(mouth_cascade.detectMultiScale(roi_gray))
+
+    return mouths
+
 def get_eyes_mouth(file):
-    face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-    eyes_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
+    face_cascade = cv2.CascadeClassifier(FACE_PATH)
+    eyes_cascade = cv2.CascadeClassifier(EYE_PATH)
+    mouth_cascade = cv2.CascadeClassifier(MOUTH_PATH)
 	
     image = cv2.imread(file)
     
     faces = get_faces(image, face_cascade)
     
     left_eyes, right_eyes = crop_eyes(image, faces, eyes_cascade)
-
-    for index, eye_roi in enumerate(left_eyes):
-        cv2.imwrite(f'cropped_left_eye_{index + 1}.png', eye_roi)
-
-    for index, eye_roi in enumerate(right_eyes):
-        cv2.imwrite(f'cropped_right_eye_{index + 1}.png', eye_roi)
+    mouths = crop_mouth(image, faces, mouth_cascade)
+    return left_eyes, right_eyes, mouths
 
 def euclidean_distance(p0, p1): # both tuple coordinate
     return math.sqrt((p1[0] - p0[0]) ** 2 + (p1[1] - p0[1]) ** 2)
@@ -127,7 +132,18 @@ for img_file in os.listdir(directory):
         mouth_opened = is_mouth_opened(mouth_points)
         print("is mouth opened?: " + str(mouth_opened))
 
-        n = []
-        k = get_eyes_mouth(img)
-        n.append(ImageLabelPair(left_eye_opened, k[0], right_eye_opened, k[1], mouth_opened, k[2]))
-
+        arr = []
+        imgs = get_eyes_mouth(os.path.join(directory, img_file))
+        arr.append(ImageLabelPair(
+            left_eye_opened, 
+            imgs[0][0], 
+            right_eye_opened, 
+            imgs[1][0], 
+            mouth_opened, 
+            imgs[2][0]
+        ))
+       
+        for obj in arr:
+            cv2.imshow("left eye", obj.left_eye_img)
+            cv2.imshow("right eye", obj.right_eye_img)
+            #cv2.imshow("mouth", obj.mouth_img)
