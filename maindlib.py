4c92946051ccf037ed4041efa7e8fd1f0da97f48
kangdabin <db021129@naver.com>
2023-08-10 21:51:49 +0900

    add for_paper

--- maindlib.py
+++ maindlib.py
@@ -107,125 +107,125 @@ def face_for_eye(directory = "./KaggleDataSet/train", detector = dlib.get_fronta
 
     return open_closed
 
-print("yawn_no_yawn 모델********************************************************************************")
-
-is_pickle = False
-if not is_pickle:
-    yawn_no_yawn = face_for_yawn()  # yawn 0, no_yawn 1
-    yawn_no_yawn = np.array(yawn_no_yawn)
-    with open("(Dlib)yawn_no_yawn.pickle", "wb") as f:
-        pickle.dump(yawn_no_yawn, f)
-else:
-    with open("(Dlib)yawn_no_yawn.pickle", "rb") as f:
-        yawn_no_yawn = pickle.load(f)
-
-print("len(yawn_no_yawn) = ", len(yawn_no_yawn))
-
-X = []  # 데이터
-y = []  # 라벨
-for feature, label in yawn_no_yawn:
-    X.append(feature)
-    y.append(label)  # label은 class num임
-
-X = np.array(X)
-X = X.reshape(-1, 145, 145, 3)
-
-label_bin = LabelEncoder()  # yawn 0, no_yawn 1
-label_bin.fit(y)
-y = label_bin.transform(y)
-y = np.array(y)
-
-seed = 42
-test_size = 0.30
-X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=seed, test_size=test_size)
-
-print("len(X_train) = ", len(X_train))
-print("len(X_test) = ", len(X_test))
-print("len(y_train) = ", len(y_train))
-print("len(y_test) = ", len(y_test))
-
-train_generator_m = ImageDataGenerator(rescale=1 / 255, zoom_range=0.2, horizontal_flip=True, rotation_range=30)
-# test_generator_m = ImageDataGenerator(rescale=1 / 255)
-train_generator_m = train_generator_m.flow(np.array(X_train), y_train, shuffle=False)
-# test_generator_m = test_generator_m.flow(np.array(X_test), y_test, shuffle=False)
-
-model_train = True
-
-if model_train:
-    model_m = Sequential()
-
-    model_m.add(Conv2D(256, (3, 3), activation="relu", input_shape=X_train.shape[1:]))
-    model_m.add(MaxPooling2D(2, 2))
-
-    model_m.add(Conv2D(128, (3, 3), activation="relu"))
-    model_m.add(MaxPooling2D(2, 2))
-
-    model_m.add(Conv2D(64, (3, 3), activation="relu"))
-    model_m.add(MaxPooling2D(2, 2))
-
-    model_m.add(Conv2D(32, (3, 3), activation="relu"))
-    model_m.add(MaxPooling2D(2, 2))
-
-    model_m.add(Flatten())
-    model_m.add(Dropout(0.5))
-
-    model_m.add(Dense(64, activation="relu"))
-    model_m.add(Dense(1, activation="sigmoid"))
-
-    model_m.compile(loss='mean_squared_error', metrics=["accuracy"], optimizer="adam")
-
-    model_m.summary()
-
-    # early_stopping = EarlyStopping(monitor='val_loss', mode='min', patience=100)
-    # print(len(test_generator_m), len(X_test), len(y_test))
-    history_m = model_m.fit(train_generator_m, epochs=100, validation_data=(X_test, y_test), shuffle=True,
-                            batch_size=16, steps_per_epoch=len(X_train)//16)  # steps_per_epoch는 데이터셋 개수, steps_per_epoch=len(X_train)
-
-    accuracy = history_m.history['accuracy']
-    val_accuracy = history_m.history['val_accuracy']
-    loss = history_m.history['loss']
-    val_loss = history_m.history['val_loss']
-    epochs = range(len(accuracy))
-
-    plt.clf()
-
-    print("yawn_no_yawn model accuracy")
-    plt.plot(epochs, accuracy, "b", label="trainning accuracy")
-    plt.plot(epochs, val_accuracy, "r", label="validation accuracy")
-    plt.title("Dlib accuracy(yawn, no_yawn)")
-    plt.xlabel("epochs")
-    plt.ylabel("accuracy")
-    plt.ylim(0, 1)
-    plt.legend()
-    plt.show()
-
-    print("yawn_no_yawn model loss")
-    plt.plot(epochs, loss, "b", label="trainning loss")
-    plt.plot(epochs, val_loss, "r", label="validation loss")
-    plt.title("Dlib loss(yawn, no_yawn)")
-    plt.xlabel("epochs")
-    plt.ylabel("loss")
-    plt.ylim(0, 1)
-    plt.legend()
-    plt.show()
-
-    model_m.save("(Dlib)yawn_no_yawn.model")
-else:
-    model_m = keras.models.load_model("(Dlib)yawn_no_yawn.model")
-
-y_prob = model_m.predict(X_test, verbose=0)
-prediction = np.round(y_prob[:, 0]).astype(int)
-# print("yawn_no_yawn model prediction", prediction)
-
-labels_new = ["yawn", "no_yawn"]
-
-model_m.evaluate(X_test, y_test)
-
-print(classification_report(y_test, prediction, target_names=labels_new))
+# print("yawn_no_yawn 모델********************************************************************************")
+#
+# is_pickle = False
+# if not is_pickle:
+#     yawn_no_yawn = face_for_yawn()  # yawn 0, no_yawn 1
+#     yawn_no_yawn = np.array(yawn_no_yawn)
+#     with open("(Dlib)yawn_no_yawn.pickle", "wb") as f:
+#         pickle.dump(yawn_no_yawn, f)
+# else:
+#     with open("(Dlib)yawn_no_yawn.pickle", "rb") as f:
+#         yawn_no_yawn = pickle.load(f)
+#
+# print("len(yawn_no_yawn) = ", len(yawn_no_yawn))
+#
+# X = []  # 데이터
+# y = []  # 라벨
+# for feature, label in yawn_no_yawn:
+#     X.append(feature)
+#     y.append(label)  # label은 class num임
+#
+# X = np.array(X)
+# X = X.reshape(-1, 145, 145, 3)
+#
+# label_bin = LabelEncoder()  # yawn 0, no_yawn 1
+# label_bin.fit(y)
+# y = label_bin.transform(y)
+# y = np.array(y)
+#
+# seed = 42
+# test_size = 0.30
+# X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=seed, test_size=test_size)
+#
+# print("len(X_train) = ", len(X_train))
+# print("len(X_test) = ", len(X_test))
+# print("len(y_train) = ", len(y_train))
+# print("len(y_test) = ", len(y_test))
+#
+# train_generator_m = ImageDataGenerator(rescale=1 / 255, zoom_range=0.2, horizontal_flip=True, rotation_range=30)
+# # test_generator_m = ImageDataGenerator(rescale=1 / 255)
+# train_generator_m = train_generator_m.flow(np.array(X_train), y_train, shuffle=False)
+# # test_generator_m = test_generator_m.flow(np.array(X_test), y_test, shuffle=False)
+#
+# model_train = True
+#
+# if model_train:
+#     model_m = Sequential()
+#
+#     model_m.add(Conv2D(256, (3, 3), activation="relu", input_shape=X_train.shape[1:]))
+#     model_m.add(MaxPooling2D(2, 2))
+#
+#     model_m.add(Conv2D(128, (3, 3), activation="relu"))
+#     model_m.add(MaxPooling2D(2, 2))
+#
+#     model_m.add(Conv2D(64, (3, 3), activation="relu"))
+#     model_m.add(MaxPooling2D(2, 2))
+#
+#     model_m.add(Conv2D(32, (3, 3), activation="relu"))
+#     model_m.add(MaxPooling2D(2, 2))
+#
+#     model_m.add(Flatten())
+#     model_m.add(Dropout(0.5))
+#
+#     model_m.add(Dense(64, activation="relu"))
+#     model_m.add(Dense(1, activation="sigmoid"))
+#
+#     model_m.compile(loss='mean_squared_error', metrics=["accuracy"], optimizer="adam")
+#
+#     model_m.summary()
+#
+#     # early_stopping = EarlyStopping(monitor='val_loss', mode='min', patience=100)
+#     # print(len(test_generator_m), len(X_test), len(y_test))
+#     history_m = model_m.fit(train_generator_m, epochs=100, validation_data=(X_test, y_test), shuffle=True,
+#                             batch_size=16, steps_per_epoch=len(X_train)//16)  # steps_per_epoch는 데이터셋 개수, steps_per_epoch=len(X_train)
+#
+#     accuracy = history_m.history['accuracy']
+#     val_accuracy = history_m.history['val_accuracy']
+#     loss = history_m.history['loss']
+#     val_loss = history_m.history['val_loss']
+#     epochs = range(len(accuracy))
+#
+#     plt.clf()
+#
+#     print("yawn_no_yawn model accuracy")
+#     plt.plot(epochs, accuracy, "b", label="trainning accuracy")
+#     plt.plot(epochs, val_accuracy, "r", label="validation accuracy")
+#     plt.title("Dlib accuracy(yawn, no_yawn)")
+#     plt.xlabel("epochs")
+#     plt.ylabel("accuracy")
+#     plt.ylim(0, 1)
+#     plt.legend()
+#     plt.show()
+#
+#     print("yawn_no_yawn model loss")
+#     plt.plot(epochs, loss, "b", label="trainning loss")
+#     plt.plot(epochs, val_loss, "r", label="validation loss")
+#     plt.title("Dlib loss(yawn, no_yawn)")
+#     plt.xlabel("epochs")
+#     plt.ylabel("loss")
+#     plt.ylim(0, 1)
+#     plt.legend()
+#     plt.show()
+#
+#     model_m.save("(Dlib)yawn_no_yawn.model")
+# else:
+#     model_m = keras.models.load_model("(Dlib)yawn_no_yawn.model")
+#
+# y_prob = model_m.predict(X_test, verbose=0)
+# prediction = np.round(y_prob[:, 0]).astype(int)
+# # print("yawn_no_yawn model prediction", prediction)
+#
+# labels_new = ["yawn", "no_yawn"]
+#
+# model_m.evaluate(X_test, y_test)
+#
+# print(classification_report(y_test, prediction, target_names=labels_new))
 
 print("open_closed 모델******************************************************************************************************")
 
-is_pickle = False
+is_pickle = True
 if not is_pickle:
     open_closed = face_for_eye()  # closed 2, open 3
     open_closed = np.array(open_closed)
